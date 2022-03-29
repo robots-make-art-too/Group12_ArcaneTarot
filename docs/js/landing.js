ea4a78c2d301d743742f52734ec1c284a4dadcb3
Jasmine Ly <jasminely428@gmail.com>
2022-03-29 15:16:41 -0400

    changes

---
+++ docs/js/landing.js
@@ -0,0 +1,8 @@
+function openTab(url) {
+    const link = document.createElement('a');
+    link.href = url;
+    link.target = '_blank';
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+}
